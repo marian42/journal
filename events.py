5bed4d42fa2cd3fa6e552852d3d1cd134cf045a7
Marian <mail@marian42.de>
2018-05-18 23:05:40 +0200

    Move add method to its own module

---
+++ events.py
@@ -0,0 +1,14 @@
+from model.event import Event
+from model.key import Key
+from model.keyvaluepair import KeyValuePair
+
+
+def add(summary, time, latitude = None, longitude = None, hash = None, tags = [], kvps = {}):
+	result = Event(summary = summary, time = time, latitude = latitude, longitude = longitude, hash = hash)
+	result.save()
+	for tag in tags:
+		result.add_tag(tag)
+	for key in kvps.keys():
+		db_key = Key.get_key(key)
+		kvp = KeyValuePair(key = db_key, value = kvps[key], entry = result)
+		kvp.save(force_insert=True)
--- model/event.py
+++ model/event.py
@@ -21,19 +21,6 @@ class Event(Model):
 		tag = Tag.get_tag(tag_name)
 		tag_to_entry = TagToEntry(tag = tag, entry = self)
 		tag_to_entry.save(force_insert=True)
-		
-	@staticmethod
-	def add(summary, time, latitude = None, longitude = None, hash = None, tags = [], kvps = {}):
-		result = Event(summary = summary, time = time, latitude = latitude, longitude = longitude, hash = hash)
-		result.save()
-		for tag in tags:
-			result.add_tag(tag)
-		for key in kvps.keys():
-			from key import Key
-			from keyvaluepair import KeyValuePair
-			db_key = Key.get_key(key)
-			kvp = KeyValuePair(key = db_key, value = kvps[key], entry = result)
-			kvp.save(force_insert=True)
 	
 	class Meta:
 		database = db
