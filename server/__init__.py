752e6dc03f9448c1cbb07d17e28382179a4d0b7c
Marian <mail@marian42.de>
2018-08-13 00:27:52 +0200

    Load events in the client

--- server/__init__.py
+++ server/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import jsonify
+from flask import request
 from peewee import *
 
 from database import db
@@ -12,9 +13,10 @@ from model.tag import Tag
 from model.tagtoevent import TagToEvent
 from model.image import Image
 
+import datetime
+
 from server.calendar_graph import CalendarGraph
 
-import json
 app = Flask(__name__, static_folder='../client/', static_url_path='')
 
 
@@ -31,6 +33,34 @@ def get_days():
 		graph = CalendarGraph()
 	return jsonify(graph.to_dict())
 
+
+def to_dict(event):
+	return {
+		"summary": event.summary,
+		"time": event.get_time().timestamp() * 1000
+	}
+
+
+@app.route("/api/events")
+def get_events():
+	time = request.args.get('time')
+	before = request.args.get('before') == 'true'
+	date = datetime.datetime.fromtimestamp(float(time) / 1000.0)
+	print(date)
+	n = 100
+	if before:
+		query = Event.select().where(Event.time < date).order_by(Event.time.desc())[:n]
+	else:
+		query = Event.select().where(Event.time > date).order_by(Event.time)[:n]
+	result = [to_dict(event) for event in query]
+	if before:
+		result = reversed(result)
+	
+	return jsonify(list(result))
+		
+	
+	
+	
 
 if __name__ == '__main__':
 	app.run()
