6afed329ac9002176e62726fc6ba77f376621681
Marian <mail@marian42.de>
2018-09-23 23:21:50 +0200

    Improve git importer

--- importer/gitrepo.py
+++ importer/gitrepo.py
@@ -4,6 +4,13 @@ from database import db
 import os
 
 
+def get_remote_url(repo):
+	for remote in repo.remotes:
+		for url in remote.urls:
+			return url
+	return None
+
+
 def import_repository(directory, emails=None):
 	name = directory.split("/")[-1]
 	
@@ -13,6 +20,8 @@ def import_repository(directory, emails=None):
 		print("Skipping " + name + ", as it's not a git repo.")
 		return False
 	
+	remote_url = get_remote_url(repo)
+	
 	print("Importing repository " + name + "...")
 	count = 0
 	with db.atomic():
@@ -20,10 +29,13 @@ def import_repository(directory, emails=None):
 			if emails is not None and commit.author.email not in emails:
 				continue
 			
+			kvps = {"author": commit.author, "message": commit.message, "sha": commit.hexsha}
+			if remote_url is not None:
+				kvps["url"] = remote_url + "/commit/" + commit.hexsha
 			events.add("Committed to " + name + ": " + commit.summary,
 				commit.committed_datetime,
 				tags=["git", "commit", name],
-				kvps={"author": commit.author, "message": commit.message})
+				kvps=kvps)
 			count += 1
 	
 	print("Imported " + str(count) + " commits from repository " + name + ".")
@@ -32,9 +44,12 @@ def import_repository(directory, emails=None):
 
 def import_repositories(directory="data/git/"):
 	events.prepare_import(10)
-	emails = [
-		"mail@example.com"
-	]
+	
+	emails = None
+	if os.path.isfile(directory + "emails.txt"):
+		with open(directory + "emails.txt", "r") as file:
+			emails = [email.strip() for email in file.readlines()]
+	
 	count = 0
 	for repository in [os.path.join(directory, name) for name in os.listdir(directory) if os.path.isdir(os.path.join(directory, name))]:
 		if import_repository(repository, emails):
